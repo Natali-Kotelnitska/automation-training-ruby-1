2dc53e07f26f0a1d9c3e8d8d081903921f0d3d0d
Natali Kotelnytska <nkotelnicka@gmail.com>
2024-12-12 19:06:17 +0200

    Remove redundant code in HomePage

--- web/pages/homepage.rb
+++ web/pages/homepage.rb
@@ -3,9 +3,6 @@
 module Web
   module Pages
     class HomePage < BasePage
-      element :slider, 'main rz-top-slider'
-      element :goods_section, 'rz-goods-sections.goods-sections'
-      element :sidebar, 'aside.sidebar_type_main'
     end
   end
 end
