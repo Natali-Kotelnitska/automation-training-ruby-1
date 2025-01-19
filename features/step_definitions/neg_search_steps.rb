998ff31bcc205f5c4a896bb3ea4b48e733ae3a01
Natali-Kotelnitska <nkotelnicka@gmail.com>
2025-01-19 23:10:42 +0200

    Refactor: combine search_steps and neg_search_steps into a single file

--- features/step_definitions/neg_search_steps.rb
+++
@@ -1,7 +0,0 @@
-Then('I should see a dropdown with no results message') do
-  expect(@homepage).to have_text(Constants::NO_RESULTS_MESSAGE)
-end
-
-Then('I should see the no results message') do
-  expect(@homepage.empty_results_text).to be_visible
-end
--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -30,3 +30,11 @@ end
 Then('the clear button should disappear') do
   expect(@homepage).to have_no_clear_button
 end
+
+Then('I should see a dropdown with no results message') do
+  expect(@homepage).to have_text(Constants::NO_RESULTS_MESSAGE)
+end
+
+Then('I should see the no results message') do
+  expect(@homepage.empty_results_text).to be_visible
+end
