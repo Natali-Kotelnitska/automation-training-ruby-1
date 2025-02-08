c5d5bec27e038eb0f2c524f7a79999e4aedcce06
Natali-Kotelnitska <nkotelnicka@gmail.com>
2025-02-08 19:58:21 +0200

    Logger: remove redundant code

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -1,7 +1,6 @@
 Given('Navigate to home page') do
   visit Constants::HOMEPAGE_URL
   @homepage.wait_for_page_load
-  $logger.info("Loaded the Home Page")
 end
 
 When('I search for {string} in the search field') do |search_term|
